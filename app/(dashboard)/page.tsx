cafb46fffab8ea9b7f6328f4a0e1d8a00cd59ea8
Churchil Owino <chachowino75@gmail.com>
2025-07-03 13:21:58 +0300

    Remove user button from home page

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -1,10 +1,7 @@
-import { UserButton } from "@clerk/nextjs";
-
 export default function Home() {
   return (
-    <div className="w-full flex items-center justify-between p-4">
+    <div className="w-full flex items-center justify-between">
       <h1>Finance Manager</h1>
-      <UserButton />
     </div>
   );
 }
