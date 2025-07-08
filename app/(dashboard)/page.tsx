a7cfae6fe9fe53892e19777ff99f45ef69cd5338
Churchil Owino <chachowino75@gmail.com>
2025-07-08 19:52:00 +0300

    Add button to open account creation modal

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -1,7 +1,14 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { useNewAccount } from "@/features/accounts/hooks/use-new-account";
+
 export default function Home() {
+  const { onOpen } = useNewAccount();
   return (
     <div className="w-full flex items-center justify-between">
       <h1>Finance Manager Home Page...</h1>
+      <Button onClick={onOpen}>Create Account</Button>
     </div>
   );
 }
