d203a0c88212b875b6c07d575fd97ee39e2d291b
Churchil Owino <chachowino75@gmail.com>
2025-07-08 14:55:04 +0300

    Revise home page text

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <div className="w-full flex items-center justify-between">
-      <h1>Finance Manager</h1>
+      <h1>Finance Manager Home Page...</h1>
     </div>
   );
 }
