9e61f7f03efa29a11a98c723e543bb22ae07936a
Churchil Owino <chachowino75@gmail.com>
2025-07-08 14:56:00 +0300

    Add global api error handler Export AppType for type safety through the app

--- app/api/[[...route]]/route.ts
+++ app/api/[[...route]]/route.ts
@@ -1,31 +1,26 @@
 import { Hono } from "hono";
-import { clerkMiddleware, getAuth } from "@hono/clerk-auth";
+
 import { handle } from "hono/vercel";
+
+import accounts from "./accounts";
+import { HTTPException } from "hono/http-exception";
 
 export const runtime = "edge";
 
 const app = new Hono().basePath("/api");
 
-app
-  .get("/hello", clerkMiddleware(), (c) => {
-    const auth = getAuth(c);
-    if (!auth?.userId) {
-      return c.json({
-        error: "Unauthorized!",
-      });
-    }
-    return c.json({
-      message: "Hello Next.js!",
-      userId: auth.userId,
-    });
-  })
-  .get("/hello/:testId", (c) => {
-    const testId = c.req.param("testId");
-    return c.json({
-      message: "Hello World",
-      testId: testId,
-    });
-  });
+// Global error handler for api
+app.onError((err, c) => {
+  if (err instanceof HTTPException) {
+    return err.getResponse();
+  }
+  return c.json({ error: "Internal server error" }, 500);
+});
+
+const routes = app.route("/accounts", accounts);
 
 export const GET = handle(app);
 export const POST = handle(app);
+
+// Enabling RPCs
+export type AppType = typeof routes;
