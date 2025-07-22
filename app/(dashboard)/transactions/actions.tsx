6562e867bcf932940d984e01cf91f64dc5d60b03
Churchil Owino <chachowino75@gmail.com>
2025-07-22 20:37:11 +0300

    Revise actions to use transaction hooks

--- app/(dashboard)/transactions/actions.tsx
+++ app/(dashboard)/transactions/actions.tsx
@@ -1,7 +1,7 @@
 "use client";
 
-import { useOpenAccount } from "@/features/accounts/hooks/use-open-account";
-import { useDeleteAccount } from "@/features/accounts/api/use-delete-account";
+import { useOpenTransaction } from "@/features/transactions/hooks/use-open-transaction";
+import { useDeleteTransaction } from "@/features/transactions/api/use-delete-transaction";
 
 import { Edit, MoreHorizontal, Trash } from "lucide-react";
 
@@ -19,12 +19,12 @@ type Props = {
 };
 
 export const Actions = ({ id }: Props) => {
-  const deleteMutation = useDeleteAccount(id);
+  const deleteMutation = useDeleteTransaction(id);
   const [ConfirmationDialog, confirm] = useConfirm(
     "Are you sure?",
-    "You are about to delete an account."
+    "You are about to delete this transaction."
   );
-  const { onOpen } = useOpenAccount();
+  const { onOpen } = useOpenTransaction();
 
   const handleDelete = async () => {
     const ok = await confirm();
